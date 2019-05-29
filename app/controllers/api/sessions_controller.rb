6450955fc7bc156d12d0143ed5c4c0c3d9308416
vineethsomanchi <VineethSomanchi@gmail.com>
2019-05-29 10:34:57 -0400

    Add user model, user/session controllers, and user jbuilder views

---
+++ app/controllers/api/sessions_controller.rb
@@ -0,0 +1,27 @@
+class Api::SessionsController < ApplicationController
+    
+    #Login
+    def create
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        
+        if @user
+            login(@user)
+            render 'api/users/show'
+        else
+            render json: ["Sorry, we can't find an account with this email address. Please try again or create a new account."], status: 401 #Unauthorized
+        end
+    
+    end
+  
+    #Logout
+    def destroy
+        @user = current_user
+        if @user
+            logout
+            render json: {}
+        else
+            render json: { message: 'You are not currently logged in'}, status: 404 #Page not found
+        end
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  root "static_pages#root"
+
+  #Backend controllers nested under API namespace
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy]
+  end
+  
+
+  #Root file
+  root 'static_pages#root'
+
 end
