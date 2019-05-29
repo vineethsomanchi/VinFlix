358f00b2435d7c0bf2e88b1bbde5a7eafcc90cee
vineethsomanchi <VineethSomanchi@gmail.com>
2019-05-29 12:07:03 -0400

    Add store (sample state fully built)

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as SessionApiUtil from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
+    const store = configureStore();
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Welcome to VinFlix!</h1>, root);
 });
 
+//Temporarily added directly to window for testing
 window.signup = SessionApiUtil.signup;
 window.login = SessionApiUtil.login;
 window.logout = SessionApiUtil.logout;
